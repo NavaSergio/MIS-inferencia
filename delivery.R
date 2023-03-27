ede62a4e6025cfe6f67f0a106337cde2ed5ee01f
NavaSergio <s3rgio.nava@gmail.com>
2023-03-27 13:28:39 -0600

    actualizacion de clase

--- delivery.R
+++ delivery.R
@@ -3,8 +3,21 @@
 install.packages("robustbase")
 
 data(delivery,package = "robustbase")
-
+delivery2 <-delivery
+delivery2$distance <- delivery$distance/10
 str(delivery)
+plot(delivery)
+
+
+
 modelo <- lm(delTime ~ n.prod + distance,data= delivery)
 modelo
 summary(modelo)
+anova(modelo)
+
+summary(lm(delTime ~ n.prod + distance,data= delivery2))
+
+X <- as.matrix(cbind(rep(1,25),delivery[,1:2]))
+y <- delivery$delTime
+
+solve(t(X)%*%X)  %*%t(X) %*% y
