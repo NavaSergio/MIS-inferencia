b2edf7f5cedf9c64718a25ca38d9ecdb521253fc
NavaSergio <s3rgio.nava@gmail.com>
2023-04-27 10:08:14 -0600

    boxcox

---
+++ boxcox.R
@@ -0,0 +1,17 @@
+data(mtcars)
+# Ajustar modelo lineal sin transformación
+modelo1 <- lm(mpg ~ wt, data = mtcars)
+
+# Imprimir resumen del modelo
+summary(modelo1)
+# Aplicar transformación Box-Cox
+bc_mpg <- MASS::boxcox(mpg ~ wt, data = mtcars)
+
+# Seleccionar la transformación óptima
+lambda <- bc_mpg$x[which.max(bc_mpg$y)]
+
+# Ajustar modelo lineal con transformación
+modelo2 <- lm((mpg^lambda - 1) / lambda ~ wt, data = mtcars)
+
+# Imprimir resumen del modelo
+summary(modelo2)
